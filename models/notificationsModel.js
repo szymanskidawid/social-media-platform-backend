accee2bb2c2ef6b695083e7d7caaf3fe857d117d
Dawid Szymański <dawidszymanski.dev@outlook.com>
2024-09-03 20:46:29 +0200

    fixed typo

--- models/notificationsModel.js
+++ models/notificationsModel.js
@@ -12,4 +12,4 @@ const notificationsSchema = new mongoose.Schema({
   ],
 });
 
-module.exports = mongoose.model("notiications", notificationsSchema);
+module.exports = mongoose.model("notifications", notificationsSchema);
