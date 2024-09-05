fe7b18baff6e8776613f716dc6bb95ad587ce715
Dawid Szymański <dawidszymanski.dev@outlook.com>
2024-09-05 21:21:50 +0200

    added post route for creating a person + empty routes

--- routes/peopleRoutes.js
+++ routes/peopleRoutes.js
@@ -14,4 +14,47 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/create", async (req, res) => {
+  const [
+    name,
+    surname,
+    town = "",
+    school = "",
+    work = "",
+    background_photo = "",
+    profile_photo = "",
+  ] = req.body;
+
+  if (!name || !surname) {
+    res.status(400).json({ error: "Required fields are missing." });
+  }
+
+  try {
+    const person = new People({
+      name,
+      surname,
+      full_name: name + " " + surname,
+      town,
+      school,
+      work,
+      background_photo,
+      profile_photo,
+      friends: [],
+      posts: [],
+      chats: [],
+    });
+
+    const savedPerson = await person.save();
+
+    res.status(201).json(savedPerson);
+  } catch (error) {
+    console.error("Error while creating a new person: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.post("/add-photo", async (req, res) => {});
+router.post("/add-friend", async (req, res) => {});
+router.put("/update", async (req, res) => {});
+
 module.exports = router;
