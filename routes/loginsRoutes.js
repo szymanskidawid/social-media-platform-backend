4ec622ba91a327d9316c6e356d7727255b8ecc69
Dawid Szymański <dawidszymanski.dev@outlook.com>
2024-09-06 20:27:21 +0200

    finished routes, ready for testing in frontend

--- routes/loginsRoutes.js
+++ routes/loginsRoutes.js
@@ -14,4 +14,55 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/create", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ error: "Required fields are missing." });
+  }
+
+  try {
+    const newAccount = new Logins({ email, password });
+
+    await newAccount.save();
+    res.status(201).json({ message: "Account created successfully" });
+  } catch (error) {
+    console.error("Error while creating a new account: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.put("/update-user-id/:loginId", async (req, res) => {
+  const { loginId } = req.params;
+  const { userId } = req.body;
+
+  if (!userId) {
+    return res.status(400).json({ error: "UserId is required" });
+  }
+
+  try {
+    const updatedLogin = Logins.findByIdAndUpdate(
+      loginId,
+      {
+        user_id: userId,
+      },
+      { new: save }
+    );
+
+    if (!updatedLogin) {
+      return res.status(404).json({ error: "Login not found." });
+    }
+
+    res.status(200).json({
+      message: "Login updated successfully",
+      login: updatedLogin,
+    });
+  } catch (error) {
+    console.error("Error while updating user id: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 module.exports = router;
--- routes/peopleRoutes.js
+++ routes/peopleRoutes.js
@@ -9,13 +9,13 @@ router.get("/", async (req, res) => {
     console.log(data);
     res.json(data);
   } catch (error) {
-    console.log("Cannot fetch people: ", error);
+    console.error("Cannot fetch people: ", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
 
 router.post("/create", async (req, res) => {
-  const [
+  const {
     name,
     surname,
     town = "",
@@ -23,7 +23,7 @@ router.post("/create", async (req, res) => {
     work = "",
     background_photo = "",
     profile_photo = "",
-  ] = req.body;
+  } = req.body;
 
   if (!name || !surname) {
     res.status(400).json({ error: "Required fields are missing." });
@@ -53,8 +53,100 @@ router.post("/create", async (req, res) => {
   }
 });
 
-router.post("/add-photo", async (req, res) => {});
-router.post("/add-friend", async (req, res) => {});
-router.put("/update", async (req, res) => {});
+router.post("/add-photo", async (req, res) => {
+  const { userId, photo } = req.body;
+
+  try {
+    const user = await People.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    user.photos.push(photo);
+    await user.save();
+
+    res.status(200).json({ message: "Photo added successfully" });
+  } catch (error) {
+    console.error("Error in photo upload: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.post("/add-friend", async (req, res) => {
+  const { userId, friendId } = req.body;
+
+  try {
+    const [user, friend] = await Promise.all([
+      People.findById(userId),
+      People.findById(friendId),
+    ]);
+
+    if (!user || !friend) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    if (user.friends.includes(friendId)) {
+      return res
+        .status(400)
+        .json({ error: "User is already a friend" });
+    }
+
+    user.friends.push(friendId);
+    await user.save();
+
+    if (!friend.friends.includes(userId)) {
+      friend.friends.push(userId);
+      await friend.save();
+    }
+
+    res.status(200).json({ message: "Friend added successfully" });
+  } catch (error) {
+    console.error("Error while adding a friend: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.put("/update/:id", async (req, res) => {
+  const {
+    name,
+    surname,
+    town,
+    school,
+    work,
+    background_photo,
+    profile_photo,
+  } = req.body;
+
+  try {
+    const updatedUser = People.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: name,
+        surname: surname,
+        town: town,
+        school: school,
+        work: work,
+        background_photo: background_photo,
+        profile_photo: profile_photo,
+      },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res
+      .status(200)
+      .json({
+        message: "User updated successfully",
+        user: updatedUser,
+      });
+  } catch (error) {
+    console.error("Error while updating user profile: ", error);
+    re.status(500).json({ error: "Internal server error" });
+  }
+});
 
 module.exports = router;
