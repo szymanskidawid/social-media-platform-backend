4f27e37e9971753de877fc520ce24721651e03b5
Dawid Szymański <dawidszymanski.dev@outlook.com>
2024-09-06 19:52:22 +0200

    changed console.log to error

--- routes/notificationsRoutes.js
+++ routes/notificationsRoutes.js
@@ -9,7 +9,7 @@ router.get("/", async (req, res) => {
     console.log(data);
     res.json(data);
   } catch (error) {
-    console.log("Cannot fetch notifications: ", error);
+    console.error("Cannot fetch notifications: ", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
