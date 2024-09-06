e85c493835d9c8645403452814f860553ad080d2
Dawid Szymański <dawidszymanski.dev@outlook.com>
2024-09-06 20:26:37 +0200

    updates according to new database layout

--- models/loginsModel.js
+++ models/loginsModel.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const loginsSchema = new mongoose.Schema({
-  _id: String,
+  //_id: String,
   email: String,
   password: String,
-  user_id: String,
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "people" },
 });
 
 module.exports = mongoose.model("logins", loginsSchema);
