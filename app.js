2080b199009ccf64e2cb9527fa4671938cf31c77
Dawid Szymański <dawidszymanski.dev@outlook.com>
2025-01-21 20:01:11 +0100

    small updates

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "*",
-    methods: "*",
-    allowedHeaders: "*",
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
   })
 );
 
--- routes/loginsRoutes.js
+++ routes/loginsRoutes.js
@@ -3,17 +3,6 @@ const Logins = require("../models/loginsModel");
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  try {
-    const data = await Logins.find({});
-    console.log(data);
-    res.json(data);
-  } catch (error) {
-    console.error("Cannot fetch logins: ", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
 router.post("/", async (req, res) => {
   const { email, password } = req.body;
 
