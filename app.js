4ee0df394851456e613a52a172898e74db9bdd8b
Dawid Szymański <dawidszymanski.dev@outlook.com>
2025-02-06 21:23:26 +0100

    cors & error handling fix

--- app.js
+++ app.js
@@ -16,15 +16,11 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
+    origin: "*",
+    methods: "*",
+    allowedHeaders: "*",
   })
 );
-
-app.use((req, res) => {
-  res.status(404).json({ error: "Not Found" });
-});
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Social Media Platform API!");
@@ -36,6 +32,10 @@ app.use("/logins", loginsRoutes);
 app.use("/notifications", notificationsRoutes);
 app.use("/people", peopleRoutes);
 app.use("/posts", postsRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: "Not Found" });
+});
 
 mongoose
   .connect(process.env.MONGODB)
