b7c149187d44ffeb06e1faac8ceb314fd93b85fa
Dawid Szymański <dawidszymanski.dev@outlook.com>
2025-02-06 21:23:39 +0100

    small fix to schema

--- models/loginsModel.js
+++ models/loginsModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const loginsSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  first_login: Boolean,
+  first_login: { type: Boolean, default: true },
 });
 
 module.exports = mongoose.model("logins", loginsSchema);
