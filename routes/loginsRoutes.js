d3b9ab6b43d771bcb18179821608853ef6d32aee
Dawid Szymański <dawidszymanski.dev@outlook.com>
2025-03-04 21:47:20 +0100

    added update route

--- routes/loginsRoutes.js
+++ routes/loginsRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Logins = require("../models/loginsModel");
+const { default: mongoose } = require("mongoose");
 
 const router = express.Router();
 
@@ -52,6 +53,36 @@ router.post("/create", async (req, res) => {
     res.status(201).json({ message: "Account created successfully" });
   } catch (error) {
     console.error("Error while creating a new account: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.put("/update", async (req, res) => {
+  const { loginId, /*pswrd,*/ firstLogin } = req.body;
+
+  try {
+    const objectId = new mongoose.Types.ObjectId(loginId);
+    const user = await Logins.findById(objectId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    /*if (pswrd !== "") {
+      user.password = pswrd;
+    }*/
+
+    if (typeof firstLogin === "boolean") {
+      user.first_login = firstLogin;
+    }
+
+    await user.save();
+
+    res
+      .status(200)
+      .json({ message: "Login details updated succesfully" });
+  } catch (error) {
+    console.error("Cannot update login details: ", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
