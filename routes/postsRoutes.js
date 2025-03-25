dfe5ee68c645c3887b875ab6ec473f8311a0f9f5
Dawid Szymański <dawidszymanski.dev@outlook.com>
2025-03-25 21:13:41 +0100

    added post creation route

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Posts = require("../models/postsModel");
+const { default: mongoose } = require("mongoose");
 
 const router = express.Router();
 
@@ -14,7 +15,27 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/add", async (req, res) => {});
+router.post("/add", async (req, res) => {
+  const { userId, time_posted, post_message, post_photo } = req.body;
+
+  try {
+    const objectId = new mongoose.Types.ObjectId(userId);
+
+    const post = new Posts({
+      user_id: objectId,
+      time_posted,
+      post_message,
+      post_photo,
+    });
+
+    const savedPost = await post.save();
+
+    res.status(201).json(savedPost);
+  } catch (error) {
+    console.error("Error while creating a new post: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 router.post("/addLike", async (req, res) => {});
 
