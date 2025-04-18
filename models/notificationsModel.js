a659d510750ea51b5b921899968bc0df8e13c889
Dawid Szymański <dawidszymanski.dev@outlook.com>
2025-04-18 15:58:56 +0200

    simplifying notifications model

--- models/notificationsModel.js
+++ models/notificationsModel.js
@@ -1,20 +1,16 @@
 const mongoose = require("mongoose");
 
 const notificationsSchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "people",
+  },
+
   notified_user_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "people",
   },
-  notifications: [
-    {
-      id: String,
-      type: String,
-      from: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "people",
-      },
-    },
-  ],
+  type: String,
 });
 
 module.exports = mongoose.model("notifications", notificationsSchema);
